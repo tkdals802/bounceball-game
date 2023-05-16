e2b048c19f67d3e8481b6e1a80ae4de3aaa94342
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-05-16 17:31:54 +0900

    moving box ver2

--- Assets/Scripts/MovingBox.cs
+++ Assets/Scripts/MovingBox.cs
@@ -10,15 +10,19 @@ public class MovingBox : MonoBehaviour
     [SerializeField]
     private float movingDistance;
     Vector2 startPosition;
+
     void Awake()
     {
         startPosition = transform.position;
     }
+
+    private int direction = 1;
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-        if (transform.position.x > startPosition.x + movingDistance ||
-            transform.position.x < startPosition.x)
-            speed = -speed;
+        transform.Translate(Vector2.right * speed * direction * Time.deltaTime);
+        if (transform.position.x > startPosition.x + movingDistance)
+            direction = -1;
+        else if (transform.position.x < startPosition.x)
+            direction = 1;
     }
 }
