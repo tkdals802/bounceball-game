9f4f1640a98080e8560d947d4b5357b3a6f791a9
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-06-05 09:06:19 +0900

    Revert "BlackHole GameClear Scene"
    This reverts commit 0a42965c792ad6ff9294fdbc62aa3f0d72d9ac1b.

--- Assets/Scripts/StageMenu.cs
+++ Assets/Scripts/StageMenu.cs
@@ -6,12 +6,7 @@ public class StageMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public string sceneName = "TestScene";
-    
-    public void backToSelectMenu()
-    {
-        SceneManager.LoadScene("SelectStage"); //게임 끝나고 맵선택버튼누를시 다시 맵을 고르게함 
-    }
-    public void SelectTestStage()
+     public void SelectTestStage()
     {
         SceneManager.LoadScene(sceneName);
     }
